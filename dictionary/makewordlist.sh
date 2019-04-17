e52b600dbb6470c66fbbb3af7a38aaa3df01165e
Jordan Leppert <jordanleppert@gmail.com>
2019-04-17 15:21:33 +0100

    Checked all three letter words. Output list is now sorted.

--- dictionary/makewordlist.sh
+++ dictionary/makewordlist.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Convert line endings, filter out words that aren't pure lowercase a-z and at least three letters long, trim whitespace
-tr -d '\015' < brit-a-z.txt | grep -E "^\S\S\S+\s*$" | sed -e "s/\\s+//g" > output.txt
+tr -d '\015' < brit-a-z.txt | grep -E "^\S\S\S\S+\s*$" | sed -e "s/\\s+//g" > unsortedoutput.txt
 # Append list of allowed 1-2 letter words
-cat shortwords.txt >> output.txt
+cat shortwords.txt >> unsortedoutput.txt
+cat unsortedoutput.txt | sort -u > output.txt
