0fed3e2fb3af2a781973d06e5a33d8dee383ddf8
Jordan Leppert <jordanleppert@gmail.com>
2019-04-18 13:25:13 +0100

    Improved dictionary pipeline

--- dictionary/makewordlist.sh
+++ dictionary/makewordlist.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# Convert line endings, filter out words that aren't pure lowercase a-z and at least three letters long, trim whitespace
-tr -d '\015' < brit-a-z.txt | grep -E "^\S\S\S\S+\s*$" | sed -e "s/\\s+//g" > unsortedoutput.txt
-# Append list of allowed 1-2 letter words
-cat shortwords.txt >> unsortedoutput.txt
-cat unsortedoutput.txt | sort -u > output.txt
+# Convert line endings, filter out words that aren't single words, trim whitespace, add additional words, sort, remove suppressed words
+tr -d '\015' < ORIGINAL.txt | grep -E "^\S+\s*$" | sed -e "s/\\s+//g" | cat - additional.txt | sort -u | comm -23 - suppress.txt > output.txt
